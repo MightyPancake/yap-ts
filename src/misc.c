62807e368fbca8c564854ac65cf84f26b1fcbdd0
MightyPancake <amightypancake@gmail.com>
2025-09-14 23:38:00 +0200

    better errors

--- src/misc.c
+++ src/misc.c
@@ -5,7 +5,7 @@
 void yap_print_tree(TSNode node, int depth){
     for(int i=0;i<depth;i++) printf("  ");
     const char* str = ts_node_type(node);
-    printf("%s\n", ts_node_has_error(node) ? "ERROR NODE" : str);
+    printf("%s%s"aesc_reset"\n", ts_node_is_error ? aesc_red : "", str);
     for_ts_children(node, n){
         yap_print_tree(n, depth+1);
     }
